39fc3286a77710f246dda6a6a119bf4b0c834a63
Daniel M Castillo <danielalejandromoralescastillo@gmail.com>
2025-04-05 01:18:04 -0600

    React app First

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+  //jsx - codigo similar a html
+  // tsx - codigo similar a html pero con tipos de datos
+  // tsx - es un archivo de typescript que contiene jsx
+
+import Titulo from './Titulo';
+import Card from './components/Card';
+
+function App(){
+  return <Card></Card>
+}
+
+export default App;
